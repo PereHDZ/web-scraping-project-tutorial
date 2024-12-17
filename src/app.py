5e0eac8ba2f795099bf332dbaecb9466ae3f8eab
PereHDZ <133217398+PereHDZ@users.noreply.github.com>
2024-12-17 18:20:22 +0000

    web scraping exercise

--- src/app.py
+++ src/app.py
@@ -1,8 +1,34 @@
-import os
+import time
 from bs4 import BeautifulSoup
 import requests
-import time
-import sqlite3
+import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+import re
 
+url = "https://companiesmarketcap.com/tesla/revenue/"
+response = requests.get(url, time.sleep(10))
+
+soup = ''
+if response:
+    soup = BeautifulSoup(response.text, 'html')
+
+years_HTML = soup.find_all('span', class_ = 'year')
+years = [int(year.text) for year in years_HTML]
+
+revenue_table = soup.find('table', class_='table')
+amounts = []
+
+tds = revenue_table.find_all('td')
+for td in tds:
+    amount = re.search(r'\$\S*', td.text)
+    if amount:
+        amounts.append(amount.group())
+
+amounts_no_dollarsign = [amount.replace('$', '') for amount in amounts]
+revenue = [(float(amount) * 1000000000) for amount in amounts_no_dollarsign]
+
+dataset = pd.DataFrame({'Years': years, 'Revenue': revenue})
+
+dataset.plot(x='Years', y='Revenue', kind='line', title='Tesla Revenue Over Years')
+
+plt.show()
